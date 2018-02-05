6b7be0e07053d680934654a3ced0c96189e8424d
jymmak <henryfrancisco_fernando@hotmail.com>
2018-02-04 22:48:11 -0500

    demás prsentaciones-funcionalidades

--- js/app.js
+++ js/app.js
@@ -5,5 +5,26 @@ $(".backico").click(function(){
   window.location.href = '../index.html';
 });
 $(".backico2").click(function(){
-  window.location.href = 'views/tips.html';
+  window.location.href = 'tips.html';
+});
+$(".backico3").click(function(){
+  window.location.href = 'agenda.html';
+});
+$(".backico4").click(function(){
+  window.location.href = 'sketch.html';
+});
+$(".backico5").click(function(){
+  window.location.href = 'tips.html';
+});
+$(".backico6").click(function(){
+  window.location.href = 'redes.html';
+});
+$(".backico7").click(function(){
+  window.location.href = 'sketch.html';
+});
+$(".backico8").click(function(){
+  window.location.href = 'final.html';
+});
+$(".backico9").click(function(){
+  window.location.href = 'redes.html';
 });
