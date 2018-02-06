e03c1158e77ec5553bac7d225b478cb89e0d909b
Cintia Tahirih <tahysluana@gmail.com>
2018-02-06 18:28:01 -0500

    modifica botones

--- js/app.js
+++ js/app.js
@@ -1,30 +1,44 @@
-$('.btn-secondary').click(function(){
+$('.btn-secondary').click(function() {
   window.location.href = 'views/agenda.html';
 });
-$(".backico").click(function(){
+$('.backico').click(function() {
   window.location.href = '../index.html';
 });
-$(".backico2").click(function(){
+$('.backico2').click(function() {
   window.location.href = 'tips.html';
 });
-$(".backico3").click(function(){
+$('.backico3').click(function() {
   window.location.href = 'agenda.html';
 });
-$(".backico4").click(function(){
+$('.backico4').click(function() {
   window.location.href = 'sketch.html';
 });
-$(".backico5").click(function(){
+$('.backico5').click(function() {
   window.location.href = 'tips.html';
 });
-$(".backico6").click(function(){
+$('.backico6').click(function() {
   window.location.href = 'redes.html';
 });
-$(".backico7").click(function(){
+$('.backico7').click(function() {
   window.location.href = 'sketch.html';
 });
-$(".backico8").click(function(){
+$('.backico8').click(function() {
   window.location.href = 'final.html';
 });
-$(".backico9").click(function(){
+$('.backico9').click(function() {
   window.location.href = 'redes.html';
 });
+$('#tips').click(function() {
+  window.location.href = 'views/tips.html';
+});
+$('#sketch').click(function() {
+  window.location.href = 'views/sketch.html';
+});
+$('#redes').click(function() {
+  window.location.href = 'views/redes.html';
+});
+$('#tecnicas').click(function() {
+  window.location.href = 'views/tecnicas.html';
+});
+
+
